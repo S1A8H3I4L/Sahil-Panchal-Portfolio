9fe87a22843665e2e236ff77a60d1b1110bb12f8
Sahil Panchal <sahilpanchal1818@gmail.com>
2025-07-04 14:32:54 +0530

    Update portfolio, remove .next build files

--- .next/static/chunks/src_app_page_tsx_fa0a80dd._.js
+++ .next/static/chunks/src_app_page_tsx_fa0a80dd._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_613d61a9._.js",
-  "static/chunks/src_components_a112a5c9._.js"
+  "static/chunks/node_modules_cb09e84e._.js",
+  "static/chunks/src_components_5eff16d9._.js"
 ],
     source: "dynamic"
 });
